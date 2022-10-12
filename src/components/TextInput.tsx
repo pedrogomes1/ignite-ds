e9acf4f8d83f144adf643ceb187f1a2eafc28fba
Pedro Gomes <pedro-gomes@ibm.com>
2022-10-12 15:08:24 -0300

    style: fix h-12 to input root

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -16,7 +16,7 @@ export interface TextInputIconProps {
 
 function TextInputRoot({ children }: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
       {children}
     </div>
   );
