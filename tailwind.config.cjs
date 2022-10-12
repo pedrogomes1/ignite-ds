75bf501cd4aeee2b1392f32f9771bc032b87171d
Pedro Gomes <pedro-gomes@ibm.com>
2022-10-12 15:04:07 -0300

    chore: update colors tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,17 +11,22 @@ module.exports = {
       "2xl": 32,
     },
     colors: {
+      transparent: "transparent",
       black: "#000",
       white: "#FFF",
 
-      "gray-900": "#121214",
-      "gray-800": "#202024",
-      "gray-400": "#7c7c8a",
-      "gray-200": "#c4c4cc",
-      "gray-100": "#e1e1e6",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7c7c8a",
+        200: "#c4c4cc",
+        100: "#e1e1e6",
+      },
 
-      "cyan-500": "#81d8f7",
-      "cyan-300": "#9BE1FB",
+      cyan: {
+        500: "#81d8f7",
+        300: "#9BE1FB",
+      },
     },
     extend: {
       fontFamily: {
