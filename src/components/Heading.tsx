223a3e4c54e49450cbde76f9c31bd16980496aa0
Pedro Gomes <pedro-gomes@ibm.com>
2022-11-02 11:02:58 -0300

    feat: create main index page using design system

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,25 +1,35 @@
+import { ReactNode } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
-import { ReactNode } from "react";
 
 export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ size = "md", asChild, children }: HeadingProps) {
-  const Component = asChild ? Slot : "h2";
+export function Heading({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) {
+  const Comp = asChild ? Slot : "h2";
 
   return (
-    <Component
-      className={clsx("text-gray-100 font-bold font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "md",
-        "text-2xl": size === "lg",
-      })}
+    <Comp
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "md",
+          "text-2xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
-    </Component>
+    </Comp>
   );
 }
